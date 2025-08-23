6bb7bee39362e0b1aa3beea9a8c502431dead15d
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-08-24 03:24:44 +0900

    Hide stage scrollbars when content fits

--- src/components/Stage.vue
+++ src/components/Stage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="containerEl" class="relative flex-1 min-h-0 p-2 overflow-scroll touch-none"
+  <div ref="containerEl" class="relative flex-1 min-h-0 p-2 overflow-auto touch-none"
        @wheel.prevent="onWheel"
        @pointerdown="onContainerPointerDown"
        @pointermove="onContainerPointerMove"
