2fb4e2e4de34d8162a51e657309117bdf35012bc
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-08-24 20:46:22 +0900

    Refine pixel store getter for readonly access

--- src/stores/pixelsStore.refactor.js
+++ src/stores/pixelsStore.refactor.js
@@ -3,31 +3,32 @@ import { readonly } from 'vue';
 
 export const usePixelsStore = defineStore('pixelsStore', {
   state: () => ({
-    pixelsByLayer: {},
+    _pixelsByLayer: {},
   }),
   getters: {
-    pixelsByLayer: (state) => readonly(state.pixelsByLayer),
+    pixelsByLayer: (state) => readonly(state._pixelsByLayer),
   },
   actions: {
     _ensureLayer(id) {
-      const map = this.$state.pixelsByLayer;
+      const map = this.$state._pixelsByLayer;
       if (!map[id]) {
         map[id] = new Set();
       }
       return map[id];
     },
     getPixels(id) {
-      return readonly(this._ensureLayer(id));
+      this._ensureLayer(id);
+      return readonly(this.pixelsByLayer[id]);
     },
     addPixel(id, coord) {
       this._ensureLayer(id).add(coord);
     },
     removePixel(id, coord) {
-      const set = this.$state.pixelsByLayer[id];
+      const set = this.$state._pixelsByLayer[id];
       if (set) set.delete(coord);
     },
     clearLayer(id) {
-      const set = this.$state.pixelsByLayer[id];
+      const set = this.$state._pixelsByLayer[id];
       if (set) set.clear();
     },
   },
