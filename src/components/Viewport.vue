6f7f413f526cea225ce455a870ea215f78c84832
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-08-27 19:31:56 +0900

    Update Viewport.vue

--- src/components/Viewport.vue
+++ src/components/Viewport.vue
@@ -118,7 +118,7 @@ const patternUrl = computed(() => `url(#${ensureCheckerboardPattern(document.bod
 
 const onImageLoad = () => {
     viewportStore.recalcContentSize();
-    viewportStore.setScale(stage.containScale);
+    viewportStore.setScale(stage.minScale);
     viewport.centerPosition();
 };
 
