356cba0a1499e09ba0161598e7e7c45ecea6f0fc
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-08-27 21:22:59 +0900

    Revert "feat: introduce basic layer groups"

--- src/components/Viewport.vue
+++ src/components/Viewport.vue
@@ -22,7 +22,9 @@
       <img v-show="viewportStore.display==='original'" class="absolute w-full h-full top-0 left-0 pointer-events-none block" :src="viewportStore.imageSrc" alt="source image" @load="onImageLoad" />
       <!-- 결과 레이어 -->
       <svg v-show="viewportStore.display==='result'" class="absolute w-full h-full top-0 left-0 pointer-events-none block" :viewBox="viewportStore.viewBox" preserveAspectRatio="xMidYMid meet">
-        <LayerRenderGroup :id="layerGroups.rootId" />
+        <g>
+            <path v-for="props in layers.getProperties(layers.idsBottomToTop)" :key="'pix-'+props.id" :d="layers.pathOf(props.id)" fill-rule="evenodd" shape-rendering="crispEdges" :fill="rgbaCssU32(props.color)" :visibility="props.visibility?'visible':'hidden'"></path>
+        </g>
       </svg>
       <!-- 그리드 -->
       <svg class="absolute w-full h-full top-0 left-0 pointer-events-none block" :viewBox="viewportStore.viewBox" preserveAspectRatio="xMidYMid meet">
@@ -72,10 +74,9 @@ import { useTemplateRef, computed, onMounted, onUnmounted } from 'vue';
 import { useStore } from '../stores';
 import { useService } from '../services';
 import { OVERLAY_CONFIG, GRID_STROKE_COLOR } from '@/constants';
-import { ensureCheckerboardPattern } from '../utils';
-import LayerRenderGroup from './LayerRenderGroup.vue';
+import { rgbaCssU32, ensureCheckerboardPattern } from '../utils';
 
-const { viewport: viewportStore, layers, viewportEvent: viewportEvents, layerGroups } = useStore();
+const { viewport: viewportStore, layers, viewportEvent: viewportEvents } = useStore();
 const { overlay, toolSelection: toolSelectionService, viewport } = useService();
 const viewportEl = useTemplateRef('viewportEl');
 const stage = viewportStore.stage;
