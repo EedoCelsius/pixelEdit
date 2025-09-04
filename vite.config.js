e275f7da99d9c99a269dd131c59e1c62a22988e0
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-09-05 04:26:12 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -11,7 +12,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
