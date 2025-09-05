be977e86390897778d22a697153539df0621f8c4
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-09-06 00:25:52 +0900

    Add files via upload

---
+++ src/image/layer_toolbar/index.js
@@ -0,0 +1,15 @@
+import add from './add.svg';
+import copy from './copy.svg';
+import merge from './merge.svg';
+import split from './split.svg';
+import empty from './empty.svg';
+import group from './group.svg';
+
+export default {
+  add,
+  group,
+  copy,
+  merge,
+  split,
+  empty
+};
