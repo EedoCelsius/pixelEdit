6c7c1263244f7793c8ee4868cabafd12d6b11a13
EedoCelsius <81402518+EedoCelsius@users.noreply.github.com>
2025-09-07 00:19:26 +0900

    feat: add border wand tool

--- src/constants/toolbar.js
+++ src/constants/toolbar.js
@@ -3,6 +3,7 @@ import stageIcons from '../image/stage_toolbar';
 export const WAND_TOOLS = [
   { type: 'path', name: 'Path', icon: stageIcons.path },
   { type: 'connect', name: 'Connect', icon: stageIcons.connect },
+  { type: 'border', name: 'Border', icon: stageIcons.border },
 ];
 
 export const TOOL_MODIFIERS = {
